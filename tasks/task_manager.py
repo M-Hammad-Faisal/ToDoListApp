0f850d6c3d48b0a22e2e298f55f4a75d7dbc5c48
Muhammad Hammad Faisal <hammadfaisal178@gmail.com>
2024-06-08 01:21:49 +0500

    task manager added

---
+++ tasks/task_manager.py
@@ -0,0 +1,43 @@
+import os
+import pickle
+
+
+class TaskManager:
+    def __init__(self, filename="tasks.pkl"):
+        self.filename = filename
+        self.tasks = self.load_tasks()
+
+    def add_task(self, task):
+        self.tasks.append(task)
+        self.save_tasks()
+
+    def update_task(self, task_index, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self.tasks[task_index], key, value)
+        self.save_tasks()
+
+    def delete_task(self, task_index):
+        self.tasks.pop(task_index)
+        self.save_tasks()
+
+    def sort_tasks(self, by="priority"):
+        if by == "priority":
+            self.tasks.sort(key=lambda task: task.priority)
+        elif by == "due_date":
+            self.tasks.sort(key=lambda task: task.due_date if task.due_date is not None else "")
+        elif by == "creation_date":
+            self.tasks.sort(key=lambda task: task.creation_date)
+        self.save_tasks()
+
+    def save_tasks(self):
+        with open(self.filename, "wb") as file:
+            pickle.dump(self.tasks, file)
+
+    def load_tasks(self):
+        if os.path.exists(self.filename):
+            with open(self.filename, "rb") as file:
+                return pickle.load(file)
+        return []
+
+    def __str__(self):
+        return "\n".join(str(task) for task in self.tasks)
