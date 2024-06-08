ab9a1b89a52955c7b3feec6e6fba9d8213e1f0d4
Muhammad Hammad Faisal <hammadfaisal178@gmail.com>
2024-06-08 05:07:39 +0500

    to do app updated

--- to_do_app_cli/to_do_app.py
+++ to_do_app_cli/to_do_app.py
@@ -1,59 +1,48 @@
 import argparse
-
 from tasks.task import Task
 from tasks.task_manager import TaskManager
 
-
 def main():
-    task_manager = TaskManager()
-
     parser = argparse.ArgumentParser(description="To-Do List App")
     subparsers = parser.add_subparsers(dest="command")
 
-    add_parser = subparsers.add_parser("add")
-    add_parser.add_argument("title", type=str, help="Title of the task")
-    add_parser.add_argument("--description", type=str, help="Description of the task")
-    add_parser.add_argument("--priority", type=int, help="Priority of the task")
-    add_parser.add_argument("--due_date", type=str, help="Due date of the task (YYYY-MM-DD)")
-
-    update_parser = subparsers.add_parser("update")
-    update_parser.add_argument("index", type=int, help="Index of the task to update")
-    update_parser.add_argument("--title", type=str, help="New title of the task")
-    update_parser.add_argument("--description", type=str, help="New description of the task")
-    update_parser.add_argument("--priority", type=int, help="New priority of the task")
-    update_parser.add_argument("--due_date", type=str, help="New due date of the task (YYYY-MM-DD)")
-
-    delete_parser = subparsers.add_parser("delete")
-    delete_parser.add_argument("index", type=int, help="Index of the task to delete")
-
-    sort_parser = subparsers.add_parser("sort")
-    sort_parser.add_argument("by", choices=["priority", "due_date", "creation_date"],
-                             help="Sort tasks by this criteria")
-
-    complete_parser = subparsers.add_parser("complete")
-    complete_parser.add_argument("index", type=int, help="Index of the task to mark as complete")
-
     subparsers.add_parser("view", help="View all current tasks")
 
-    args = parser.parse_args()
+    add_parser = subparsers.add_parser("add", help="Add a new task")
+    add_parser.add_argument("title", help="Title of the task")
+    add_parser.add_argument("--description", help="Description of the task", default="")
+    add_parser.add_argument("--priority", type=int, help="Priority of the task", default=0)
+    add_parser.add_argument("--due_date", help="Due date of the task", default=None)
 
-    if args.command == "add":
-        due_date = args.due_date if args.due_date else None
-        task = Task(title=args.title, description=args.description, priority=args.priority, due_date=due_date)
-        task_manager.add_task(task)
+    update_parser = subparsers.add_parser("update", help="Update an existing task")
+    update_parser.add_argument("index", type=int, help="Index of the task to update")
+    update_parser.add_argument("--title", help="New title of the task")
+    update_parser.add_argument("--description", help="New description of the task")
+    update_parser.add_argument("--priority", type=int, help="New priority of the task")
+    update_parser.add_argument("--due_date", help="New due date of the task")
+
+    subparsers.add_parser("delete", help="Delete a task").add_argument("index", type=int, help="Index of the task to delete")
+    subparsers.add_parser("complete", help="Mark a task as complete").add_argument("index", type=int, help="Index of the task to mark as complete")
+    subparsers.add_parser("sort", help="Sort tasks").add_argument("by", choices=["priority", "due_date", "creation_date"], help="Sort tasks by this field")
+
+    args = parser.parse_args()
+    task_manager = TaskManager()
+
+    if args.command == "view":
+        print(task_manager)
+    elif args.command == "add":
+        new_task = Task(args.title, args.description, args.priority, args.due_date)
+        task_manager.add_task(new_task)
     elif args.command == "update":
-        kwargs = {k: v for k, v in vars(args).items() if k not in ["index", "command"] and v is not None}
-        task_manager.update_task(args.index, **kwargs)
+        update_fields = {k: v for k, v in vars(args).items() if k != "command" and k != "index" and v is not None}
+        task_manager.update_task(args.index, **update_fields)
     elif args.command == "delete":
         task_manager.delete_task(args.index)
-    elif args.command == "sort":
-        task_manager.sort_tasks(by=args.by)
     elif args.command == "complete":
         task_manager.tasks[args.index].mark_complete()
         task_manager.save_tasks()
-    elif args.command == "view":
-        print(task_manager)
-
+    elif args.command == "sort":
+        task_manager.sort_tasks(by=args.by)
 
 if __name__ == "__main__":
     main()
